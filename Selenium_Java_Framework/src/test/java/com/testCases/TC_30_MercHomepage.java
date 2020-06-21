b4b268de9ddbac9dfe12da2ceb0998ea5c224b2c
ankushingole <56147964+ankushingole@users.noreply.github.com>
2020-06-21 17:44:45 +0530

    New test case added

---
+++ Selenium_Java_Framework/src/test/java/com/testCases/TC_30_MercHomepage.java
@@ -0,0 +1,24 @@
+package com.testCases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC_30_MercHomepage {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		driver.get("https://www.mercedes-benz.co.in/?group=all&subgroup=see-all&view=BODYTYPE");
+		String title = driver.getTitle();
+		System.out.println(title);
+
+		driver.quit();
+	}
+
+}
