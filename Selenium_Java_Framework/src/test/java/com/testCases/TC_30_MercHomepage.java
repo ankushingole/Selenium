b79271e8526f4a08fb9fcbe5c87a33bb8559aa8b
ankushingole <56147964+ankushingole@users.noreply.github.com>
2020-06-21 17:52:09 +0530

    second commit

--- Selenium_Java_Framework/src/test/java/com/testCases/TC_30_MercHomepage.java
+++ Selenium_Java_Framework/src/test/java/com/testCases/TC_30_MercHomepage.java
@@ -13,7 +13,11 @@ public class TC_30_MercHomepage {
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
 		driver.get("https://www.mercedes-benz.co.in/?group=all&subgroup=see-all&view=BODYTYPE");
+		String title = driver.getTitle();
+		System.out.println(title);
+
 		driver.quit();
 	}
 
